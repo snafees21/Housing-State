664a5ad97f38630f89a859cb30d120e6d5df328f
kameliashaharova <60710547+kameliashaharova@users.noreply.github.com>
2020-06-16 00:05:38 -0700

    Create kamelia.js

---
+++ application/frontend/src/components/about/userPages/kamelia.js
@@ -0,0 +1,18 @@
+import { bioKamelia } from '../../../utils/text';
+
+const kamelia = () => {
+    return (
+        <React.Fragment>
+            <center>
+                <div>
+                    <h1>Kamelia</h1>
+                </div>
+                <div>
+                    <p>{biokamelia}</p>
+                </div>
+            </center>
+        </React.Fragment>
+    );
+  };
+  
+  export default kamelia;
