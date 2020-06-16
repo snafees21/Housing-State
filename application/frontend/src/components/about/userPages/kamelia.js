57f1a8c9091b08e6a4903cc58e9c121767a23a09
kameliashaharova <60710547+kameliashaharova@users.noreply.github.com>
2020-06-15 23:43:12 -0700

    Update and rename garrett.js to kamelia.js

--- application/frontend/src/components/about/userPages/kamelia.js
+++ application/frontend/src/components/about/userPages/kamelia.js
@@ -1,18 +1,18 @@
-import { bioGarrett } from '../../../utils/text';
+import { bioKamelia } from '../../../utils/text';
 
 const garrett = () => {
     return (
         <React.Fragment>
             <center>
                 <div>
-                    <h1>garrett</h1>
+                    <h1>Kamelia</h1>
                 </div>
                 <div>
-                    <p>{bioGarrett}</p>
+                    <p>{bioKamelia}</p>
                 </div>
             </center>
         </React.Fragment>
     );
   };
   
-  export default garrett;
+  export default kamelia;
