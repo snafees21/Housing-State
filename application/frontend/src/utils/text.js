44ee44206fb72e977d25ef54b4c2bafa4eb68885
kameliashaharova <60710547+kameliashaharova@users.noreply.github.com>
2020-06-16 13:55:39 -0700

    update text.js — added more to bio

--- application/frontend/src/utils/text.js
+++ application/frontend/src/utils/text.js
@@ -8,7 +8,10 @@ const bioNick = '';
 
 const bioSarah = '';
 
-const bioKamelia = 'Hi, my name is Kamelia Shaharova! I will be a senior this Fall semester at SFSU.';
+const bioKamelia = 'Hello! My name is Kamelia Shaharova, I will be a senior this upcoming Fall semester at SFSU. \
+                    I am interested in data science and web development, and am currently exploring my options \
+                    but I am very excited about my future and its possibilities. In my free time I enjoy \
+                    running, cooking, and skiing!';
 
 const bioMantasha = '';
 
