525b4c86ea9563c8569d5fc97da3c2dea8540a76
Garrett Johnson <garrett.johnson@keeptruckin.com>
2020-06-15 23:35:44 -0700

    completed a template that other group members can use for their about pages, added responsive font resizing in scss

--- application/frontend/src/components/App/App.js
+++ application/frontend/src/components/App/App.js
@@ -1,24 +1,32 @@
 import Home from '../home/home';
 import About from '../about/about';
 import Navbar from '../../utils/navbar';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom'; import Garrett from '../about/userPages/garrett';
 
 
 const App = ({ }) => {
     return (
         <React.Fragment>
-            <Navbar/>
-            
-            <Switch>
-                <Route path="/about">
-                    <About />
-                </Route>
+            <div>
+                <header>
+                    <Navbar/>
+                </header>
+                <div>
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
 
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-            
+                        <Route path="/garrett">
+                            <Garrett />
+                        </Route>
+
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
         </React.Fragment> 
     );
   }
--- application/frontend/src/utils/text.js
+++ application/frontend/src/utils/text.js
@@ -1,6 +1,9 @@
 // Text for about page defined here. This information may eventually live in our SQL db
 
-const bioGarrett = 'Hi! My name is Garrett Johnson. I am a senior at SFSU.';
+const bioGarrett = 'Hi! My name is Garrett Johnson. I am a senior at SFSU studying computer science. \
+                    I currently work as an analyst, and am considering a career in data science in the \
+                    future. I am excited to be team lead on this project and be able to share some of \
+                    the experience I have gained at my job with the rest of my team.';
 
 const bioBuu = '';
 
