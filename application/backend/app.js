92ea2a97a9fbab8e35a80b26e992c48b761a7140
Garrett Johnson <garrett.johnson@keeptruckin.com>
2020-07-08 22:46:17 -0700

    small spacing/formatting changes

--- application/backend/app.js
+++ application/backend/app.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/api/user', user);
 app.use('/api/listing', listing);
 app.use('/api/message', message);
