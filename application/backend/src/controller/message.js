d14f0717ff3b920814b745b241cce3bd6bab9c5a
mantashakhan <55900867+mantashakhan@users.noreply.github.com>
2020-07-23 15:21:51 -0700

    Add files via upload
    recent message functionality added
    created api to show all the chats in chatlist

--- application/backend/src/controller/message.js
+++ application/backend/src/controller/message.js
@@ -1,11 +1,41 @@
-const Message = require('../models/message');
-
+const Sequelize = require('sequelize');
+const db = require("../models");
+const Message = db.Message;
 // route: GET /api/message
 exports.getMessages = async (req, res, next) => {
-  res.send('GET message'); // TODO: replace with actual code
+  try {
+    // eslint-disable-next-line camelcase
+    const { chat_id } = req.query;
+    const attributes = { exclude: ['password'] };
+    const messages = await Message.findAll({
+      where:{
+        chat_id,
+      },
+      order: [
+        ['createdAt', 'DESC'],
+      ],
+      include: [{
+        model: db.User,
+        attributes,
+        as: 'sender',
+      },{
+        model: db.User,
+        attributes,
+        as: 'receiver',
+      }]
+    });
+    res.status(200).json(messages);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
 };
 
 // route: POST /api/message
 exports.addMessages = async (req, res, next) => {
-  res.send('POST message'); // TODO: replace with actual code
+  try {
+    const msg = await Message.create(req.body);
+    res.status(200).send('success');
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
 };
