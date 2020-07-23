8afdf1163fec34ba001a358c18d54a14a9c04dd7
mantashakhan <55900867+mantashakhan@users.noreply.github.com>
2020-07-23 15:42:57 -0700

    Delete chatList.js

--- application/backend/routes/chatList.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getChatList, addChatList } from '../controller/chatList';
-
-const message = express.Router();
-
-message.route('/').get(getChatList).post(addChatList);
-
-export default message;
