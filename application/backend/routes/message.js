a755a5fe4dd5d55ce62e810974554161af064abe
mantashakhan <55900867+mantashakhan@users.noreply.github.com>
2020-07-23 15:43:20 -0700

    Delete message.js

--- application/backend/routes/message.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getMessages, addMessages } from '../controller/message';
-
-const message = express.Router();
-
-message.route('/').get(getMessages).post(addMessages);
-
-export default message;
