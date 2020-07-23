480ba06eef516e6d8413506216a3dc9238728146
mantashakhan <55900867+mantashakhan@users.noreply.github.com>
2020-07-23 15:50:21 -0700

    Add files via upload

--- application/backend/src/models/message.js
+++ application/backend/src/models/message.js
@@ -1,39 +1,71 @@
 import db from '../database/db';
+
 const Sequelize = require('sequelize');
 
 // defines schema for messages table ('s' added by sequelize)
-const Message = db.define('Message', {
-  id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-
-  from_user: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Users',
-      key: 'id',
-      comment: 'foreign key, from `users` table, sender of the message',
+module.exports = (sequelize, Sequelize) => {
+  const Message = sequelize.define('Message', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-  },
 
-  to_user: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'users',
-      key: 'id',
-      comment: 'foreign key, from `users` table, recipient of the message',
+    from_user: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+        comment: 'foreign key, from `users` table, sender of the message',
+      },
     },
-  },
 
-  text: {
-    type: Sequelize.STRING(1000),
-    allowNull: false,
-  },
-});
+    to_user: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+        comment: 'foreign key, from `users` table, recipient of the message',
+      },
+    },
 
-module.exports = Message;
+    text: {
+      type: Sequelize.STRING(1000),
+      allowNull: false,
+    },
+    chat_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'chat_lists',
+        key: 'id',
+      },
+    },
+  });
+
+  Message.associate = function (models) {
+    Message.belongsTo(models.User, {
+      as: 'sender',
+      foreignKeyConstraint: true,
+      foreignKey: 'from_user',
+      targetKey: 'id'
+    });
+    Message.belongsTo(models.User, {
+      as: 'receiver',
+      foreignKeyConstraint: true,
+      foreignKey: 'to_user',
+      targetKey: 'id'
+    });
+    Message.belongsTo(models.chat_list, {
+      foreignKeyConstraint: true,
+      foreignKey: 'chat_id',
+      targetKey: 'id'
+    });
+  };
+  return Message;
+};
+
+// module.exports = Message;
