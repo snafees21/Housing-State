0e2db5184671f8902112cfae53fe70c9dc9800a8
mantashakhan <55900867+mantashakhan@users.noreply.github.com>
2020-07-23 15:43:05 -0700

    Delete index.js

--- application/backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-import user from './user';
-import listing from './listing';
-import message from './message';
-import chatList from './chatList';
-
-export {
-  user, listing, message, chatList,
-};
