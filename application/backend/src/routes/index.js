5f9d8a6d0d976034e10238955e2326ba9c054f24
mantashakhan <55900867+mantashakhan@users.noreply.github.com>
2020-07-23 15:51:19 -0700

    Add files via upload

--- application/backend/src/routes/index.js
+++ application/backend/src/routes/index.js
@@ -1,5 +1,8 @@
 import user from './user';
 import listing from './listing';
 import message from './message';
+import chatList from './chatList';
 
-export { user, listing, message };
+export {
+  user, listing, message, chatList,
+};
