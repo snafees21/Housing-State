f32796f7f625e713c03c058f3cf2a07a72fba2d2
buuphan36 <buuphan739@gmail.com>
2020-07-28 13:15:49 -0700

    added remove function request

--- application/frontend/src/components/message/showRecent.js
+++ application/frontend/src/components/message/showRecent.js
@@ -1,10 +1,21 @@
 const showRecent = ({ name, message }) => {
+  const deleteMessage = () => {
+    /*axios
+    .get('/api/deleteMessage', { params: query })
+    .then((res) => {
+    })
+    .catch(() => {
+      console.log("Failed");
+    });
+    */
+  };
   return (
     <React.Fragment>
       <div class="card m-2">
         <div class="card container">
           <legend>{name}</legend>
           <p>{message}</p>
+          
           <span class="text-right">11:30 PM</span>
         </div>
       </div>
