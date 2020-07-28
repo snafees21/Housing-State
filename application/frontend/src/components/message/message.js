4d79888097716b8bb53aeaeba6dae4e46398b0ce
buuphan36 <buuphan739@gmail.com>
2020-07-27 18:35:33 -0700

    Fixed misused CSS

--- application/frontend/src/components/message/message.js
+++ application/frontend/src/components/message/message.js
@@ -2,7 +2,7 @@ const message = ({ from_user, from_user_message, userID, time }) => {
   const factory = () => {
     if (from_user === userID) {
       return (
-        <div class="float-right card bg-primary message-message">
+        <div class="card bg-primary message-reply">
           <div class="text-light font-weight-bold message-no-padding">
             {from_user}
             <div class="float-right text-dark text-secondary ml-5">{time} </div>
