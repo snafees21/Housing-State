86e0a481896e262446a50cef79440bcac93d1cf4
Kamelia Shaharova <60710547+kameliashaharova@users.noreply.github.com>
2020-08-05 13:41:46 -0700

    Update message.js

--- application/backend/src/controller/message.js
+++ application/backend/src/controller/message.js
@@ -1,49 +1,11 @@
 const Message = require('../models/message');
-const Sequelize = require('sequelize');
 
-// route: GET /api/message/:from_user
-exports.getConversationsWith = async (req, res, next) => {
-  try {
-    const conversations = await Message.findAll({
-      group: ['to_user'],
-      attributes: [
-        'to_user',
-        [Sequelize.fn('max', Sequelize.col('createdAt')), 'last_message_at'],
-      ],
-      where: { from_user: req.params.from_user },
-      order: [[Sequelize.col('last_message_at'), 'DESC']],
-    });
-    res.send(conversations);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-};
-
-// route: GET /api/message/:from_user/:to_user
+// route: GET /api/message
 exports.getMessages = async (req, res, next) => {
-  try {
-    const messages = await Message.findAll({
-      where: { from_user: req.params.from_user, to_user: req.params.to_user },
-      order: [['createdAt', 'DESC']],
-    });
-    res.send(messages);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
+  res.send('GET message'); // TODO: replace with actual code
 };
 
 // route: POST /api/message
-exports.addMessage = async (req, res, next) => {
-  // unpacks key, values of req.body to provide
-  // {column: value} association
-  try {
-    console.log(req.body);
-    await Message.create(req.body);
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
+exports.addMessages = async (req, res, next) => {
+  res.send('POST message'); // TODO: replace with actual code
 };
