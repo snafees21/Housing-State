a42f98f587853c6806e9356b437f52d4ecc66868
Kamelia Shaharova <60710547+kameliashaharova@users.noreply.github.com>
2020-08-05 13:41:58 -0700

    Update message.js

--- application/backend/src/controller/message.js
+++ application/backend/src/controller/message.js
@@ -1,11 +1,38 @@
-const Message = require('../models/message');
+const User = require('../models/user');
 
-// route: GET /api/message
-exports.getMessages = async (req, res, next) => {
-  res.send('GET message'); // TODO: replace with actual code
+// route: GET /api/user
+exports.getUsers = async (req, res, next) => {
+  res.send('GET user'); // TODO: replace with actual code
 };
 
-// route: POST /api/message
-exports.addMessages = async (req, res, next) => {
-  res.send('POST message'); // TODO: replace with actual code
+// route: GET /api/user/:id
+exports.validateUser = async (req, res, next) => {
+  // return if user has sfsu email and if they are an admin
+  res.send('POST user'); // TODO: replace with actual code
+};
+
+// route: POST /api/user
+exports.addUser = async (req, res, next) => {
+  res.send('POST user'); // TODO: replace with actual code
+};
+
+// route: POST /api/user/auth
+exports.authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: res.body.email } });
+    if (!user) {
+      res.send({ sucess: false, message: 'Invalid Email' });
+    } else if (!user.validPassword(res.body.password)) {
+      res.send({ sucess: false, message: 'Invalid Password' });
+    } else {
+      res.send({ sucess: true, message: null });
+    }
+  } catch (error) {
+    res.sendStatus(500);
+  }
+};
+
+// route: DELETE /api/user/:id
+exports.deleteUsers = async (req, res, next) => {
+  res.send('DELETE user'); // TODO: replace with actual code
 };
