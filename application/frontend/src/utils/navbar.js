5f0dae96e76d1651813b8d9007c614478554597a
snafees21 <snafees@mail.sfsu.edu>
2020-08-05 18:41:22 -0700

    Positoned logo above title, moved About Us to the left, fixed header and navbar

--- application/frontend/src/utils/navbar.js
+++ application/frontend/src/utils/navbar.js
@@ -3,18 +3,24 @@ const navbar = () => {
   // and render Account when authTokens exist
  
   return (
-  <div className="navbar-light bg">
-    <div className="header">
+    <div className="navbar-light bg">
+    <div className="header logo">
+      <img src="https://raw.githubusercontent.com/CSC-648-SFSU/csc648-su20-team5/page-title-logo/application/frontend/src/assets/imgs/logo.jpg?token=AOPF5EZT72CMBRDXZFPQOKK7F6JFS" 
+     width="80" height="73"></img> 
     <h1>Housing State </h1>
     <p>SFSU Software Engineering Project CSC 648-848, Summer 2020 Team 5. For Demonstration Only.</p>
-    <div className="logo">
-     <img src="https://raw.githubusercontent.com/CSC-648-SFSU/csc648-su20-team5/page-title-logo/application/frontend/src/assets/imgs/logo.jpg?token=AOPF5EZT72CMBRDXZFPQOKK7F6JFS" 
-     width="80" height="73"></img> 
-     </div>
      </div>
   
-
       <nav className='navbar navbar-expand-md navbar-fixed-top justify-content-end navbar-light bg'>
+      <div className='navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2'>
+        <ul className='navbar-nav mr-auto'>
+        <li className='nav-item'>
+            <a className='nav-link text-white' href='/about'>
+              About Us  
+            </a>
+          </li>
+        </ul>
+      </div>
       <div className='ml-auto navbar-brand order-0'>
         <button
           className='navbar-toggler'
@@ -32,12 +38,6 @@ const navbar = () => {
             <a className='nav-link text-white' href='/'>
               Home
             </a>  
-          </li>
-
-          <li className='nav-item'>
-            <a className='nav-link text-white' href='/about'>
-              About Us
-            </a>
           </li>
         </ul>
       </div>
