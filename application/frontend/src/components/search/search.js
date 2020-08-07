abe5e48e86aaf605717025f43985d622c37a0e16
Garrett Johnson <garrett.johnson@keeptruckin.com>
2020-08-07 13:55:14 -0700

    removed advanced search functionality to get basic search working again

--- application/frontend/src/components/search/search.js
+++ application/frontend/src/components/search/search.js
@@ -35,7 +35,7 @@ const searchbar = ({ setListings }) => {
         }}
       >
         {({ isSubmitting }) => (
-          <div>
+          <Form>
             <div className='form-row'>
               <TextInput
                 name='search_term'
@@ -60,81 +60,81 @@ const searchbar = ({ setListings }) => {
                   >
                     Options
                   </button>
+
                   <div className='dropdown-menu' role='menu'>
-                    <Form>
-                      <div className='mt-4 mb-1'>
-                        <center className='text-muted'>Advanced Search</center>
+                    <div className='mt-4 mb-1'>
+                      <center className='text-muted'>Advanced Search</center>
+                    </div>
+
+                    <div className='card-body'>
+                      <div className='form-group text-left'>
+                        <label className='label-text' htmlFor='unit_type'>
+                          Unit Type
+                        </label>
+                        <Select
+                          id='unit_type'
+                          name='unit_type'
+                          className='form-control border drop-text'
+                        >
+                          <option value=''>All</option>
+                          <option value='house'>House</option>
+                          <option value='appartment'>Apartment</option>
+                          <option value='townhouse'>Townhouse</option>
+                        </Select>
                       </div>
-                      <div className='card-body'>
-                        <div className='form-group text-left'>
-                          <label className='label-text' htmlFor='unit_type'>
-                            Unit Type
-                          </label>
-                          <Select
-                            id='unit_type'
-                            name='unit_type'
-                            className='form-control border drop-text'
-                          >
-                            <option value=''>All</option>
-                            <option value='house'>House</option>
-                            <option value='appartment'>Apartment</option>
-                            <option value='townhouse'>Townhouse</option>
-                          </Select>
-                        </div>
-                        <div className='form-group text-left'>
-                          <label className='label-text' htmlFor='offer_type'>
-                            Offer Type
-                          </label>
-                          <Select
-                            id='offer_type'
-                            name='offer_type'
-                            className='form-control border drop-text'
-                          >
-                            <option value=''>All</option>
-                            <option value='buy'>Buy</option>
-                            <option value='rent'>Rent</option>
-                          </Select>
-                        </div>
-                        <div className='form-group text-left'>
-                          <label className='label-text' htmlFor='bedrooms'>
-                            Bedrooms
-                          </label>
-                          <Select
-                            id='bedrooms'
-                            name='bedrooms'
-                            className='form-control border drop-text'
-                          >
-                            <option value=''>All</option>
-                            <option value='1'>1+</option>
-                            <option value='2'>2+</option>
-                            <option value='3'>3+</option>
-                            <option value='4'>4+</option>
-                            <option value='5'>5+</option>
-                          </Select>
-                        </div>
-                        <div className='form-group text-left'>
-                          <label className='label-text' htmlFor='cost'>
-                            Cost
-                          </label>
-                          <Select
-                            id='cost'
-                            name='cost'
-                            className='form-control border drop-text'
-                          >
-                            <option value=''>Any</option>
-                            <option value='750'>$750+</option>
-                            <option value='1000'>$1000+</option>
-                            <option value='1500'>$1500+</option>
-                            <option value='3000'>$3000+</option>
-                          </Select>
-                        </div>
+                      <div className='form-group text-left'>
+                        <label className='label-text' htmlFor='offer_type'>
+                          Offer Type
+                        </label>
+                        <Select
+                          id='offer_type'
+                          name='offer_type'
+                          className='form-control border drop-text'
+                        >
+                          <option value=''>All</option>
+                          <option value='buy'>Buy</option>
+                          <option value='rent'>Rent</option>
+                        </Select>
                       </div>
-                    </Form>
+                      <div className='form-group text-left'>
+                        <label className='label-text' htmlFor='bedrooms'>
+                          Bedrooms
+                        </label>
+                        <Select
+                          id='bedrooms'
+                          name='bedrooms'
+                          className='form-control border drop-text'
+                        >
+                          <option value=''>All</option>
+                          <option value='1'>1+</option>
+                          <option value='2'>2+</option>
+                          <option value='3'>3+</option>
+                          <option value='4'>4+</option>
+                          <option value='5'>5+</option>
+                        </Select>
+                      </div>
+                      <div className='form-group text-left'>
+                        <label className='label-text' htmlFor='cost'>
+                          Cost
+                        </label>
+                        <Select
+                          id='cost'
+                          name='cost'
+                          className='form-control border drop-text'
+                        >
+                          <option value=''>Any</option>
+                          <option value='750'>$750+</option>
+                          <option value='1000'>$1000+</option>
+                          <option value='1500'>$1500+</option>
+                          <option value='3000'>$3000+</option>
+                        </Select>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Form>
         )}
       </Formik>
     </div>
